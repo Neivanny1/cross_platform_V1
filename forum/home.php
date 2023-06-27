61f602442ebb3e4069a4a6fc7c170364c7f64928
Neivanny1 <nevileneivanny@gmail.com>
2023-06-27 19:20:09 +0300

    Incomplete project V1

---
+++ forum/home.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="description" content="Maseno All learning Materials Center">
+	  <meta name="keywords" content="Research Platform,Q&A Zone, Ideology CrossPlatform">
+  	<meta name="author" content="Erick Mwakazi">
+    <title>Maseno CrossPlatform</title>
+    <link rel="stylesheet" href="../css/css.css">
+    <link rel="icon" href="../img/favicon.png">
+  </head>
+  <body>
+    <header>
+      <div class="container">
+        <div id="branding">
+          <h1><span class="highlight">Maseno</span> CrossPlatform</h1>
+        </div>
+        <nav>
+          <ul>
+            <li class="current"><a href="resources.html">Home</a></li>
+            <li><a href="about.html">About</a></li>
+            <li><a href="../forum/index.php">Forum</a></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+
+    <section id="showcase">
+     
+      <div id="contain" class="container">
+       
+        <h1>Welcome To Maseno CrossPlatform.</h1>
+        <p>Build To make the Impossible Happen.</p>
+        <p>We equip you all necessary tools and materials to achieve your Goals and make your Ideas explore</p>
+   
+      </div>
+      
+      
+    </section>
+
+    <section id="newsletter">
+      <div class="container">
+        <h1>Subscribe To Our Website</h1>
+        <form>
+          <input type="email" placeholder="Enter Email..." required>
+          <button type="submit" class="button_1">Subscribe</button>
+        </form>
+      </div>
+    </section>
+
+    <section id="boxes">
+      <div class="container">
+        <div class="box">
+          <img src="../img/logo_html.png">
+          <h3>Online Video Tutorials</h3>
+          <p>Link me to the Tutorials </p>
+        </div>
+        <div class="box">
+          <img src="../img/logo_css.png">
+          <h3>Written Resources</h3>
+          <p>Visit the Resources site</p>
+        </div>
+        <div class="box">
+          <img src="../img/logo_brush.png">
+          <h3>Forum Section</h3>
+          <p><a href="../forum/index.php">Meet Colleagues With Ideas Like You</a> </p>
+        </div>
+      </div>
+    </section>
+
+    <footer>
+      <p>Maseno CrossPlatform, Copyright &copy; 2022</p>
+    </footer>
+  </body>
+</html>
